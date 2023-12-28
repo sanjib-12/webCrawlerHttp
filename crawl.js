c86cfcb43037eac9e11c446184d55fd8292833a2
sanjib <sanjibbarakoti112@gmail.com>
2023-12-27 21:08:45 -0500

    crawl page recusive working

--- crawl.js
+++ crawl.js
@@ -1,31 +1,56 @@
 const{JSDOM} = require('jsdom');
 const fs = require('fs');
 
-async function crawlPage(currentURL){
-    console.log(`actively crawling : ${currentURL}`)
+async function crawlPage(baseURL, currentURL, pages){
+    
+
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`actively crawling : ${currentURL}`);
     try{
         const resp = await fetch(currentURL)
         if(resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-            return;
+            return pages;
         }
         const contentType = resp.headers.get("content-type");
         if(!contentType.includes("text/html")){
             console.log(`non html response: ${resp.status} on page: ${currentURL}`);
-            return;
+            return pages;
         }
-        const output = await resp.text();
+
+        const htmlBody = await resp.text();
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     
-        fs.writeFile('output.txt',output,(err)=>{
-            if(err){
-                console.log(err);
-            }
-        })
+        // fs.writeFile('output.txt',htmlBody,(err)=>{
+        //     if(err){
+        //         console.log(err);
+        //     }
+        // })
         
     }catch(err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
     
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
